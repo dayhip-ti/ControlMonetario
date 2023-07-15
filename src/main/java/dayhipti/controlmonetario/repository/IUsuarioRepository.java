a5f866171f298825a7fd016647e715832b66b739
Dayhip Nuñez <dayhip.ti@gmail.com>
2023-07-15 01:35:30 -0400

    usuario recuperado

--- src/main/java/dayhipti/controlmonetario/repository/IUsuarioRepository.java
+++ src/main/java/dayhipti/controlmonetario/repository/IUsuarioRepository.java
@@ -1,16 +1,7 @@
 package dayhipti.controlmonetario.repository;
 
-<<<<<<< HEAD
 import dayhipti.controlmonetario.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 
 public interface IUsuarioRepository extends JpaRepository<Usuario,String> {
-=======
-
-import org.springframework.stereotype.Service;
-
-
-public interface IUsuarioRepository {
-
 }

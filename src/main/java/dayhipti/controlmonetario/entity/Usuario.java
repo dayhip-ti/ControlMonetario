549a4f6b999fc9ef1a24a1b991ba940a75b1882f
Dayhip Nuñez <dayhip.ti@gmail.com>
2023-07-15 00:15:00 -0400

    Entity Usuario

--- src/main/java/dayhipti/controlmonetario/entity/Usuario.java
+++ src/main/java/dayhipti/controlmonetario/entity/Usuario.java
@@ -1,4 +1,33 @@
 package dayhipti.controlmonetario.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Column
+    private int perfil_adm;
+    @Id
+    @Column(length = 12,nullable = false)
+    private String rut;
+    @Column(columnDefinition = "text",nullable = false)
+    private String nombre;
+    @Column(columnDefinition = "text",nullable = false)
+    private String correo;
+    @Column(columnDefinition = "text",nullable = false)
+    private String clave;
+    @Column(columnDefinition = "text",nullable = false)
+    private String nivel;
+    @Column(columnDefinition = "text",nullable = false)
+    private String numeroNivel;
+    @Column(columnDefinition = "text",nullable = false, name = "letra_Nivel")
+    private String letraNivel;
+    @Column(columnDefinition = "text",nullable = false)
+    private String escuelas;
+    @ManyToOne(optional = false,fetch = FetchType.EAGER)
+            @JoinColumn(name = "id_perfil_usuario",nullable = false, columnDefinition = "varchar(2)")
+    private Perfil perfil;
+
 }

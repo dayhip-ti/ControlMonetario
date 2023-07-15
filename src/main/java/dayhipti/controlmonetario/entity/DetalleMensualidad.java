30f390239524decb82cac32d38e22e39f6ab6d32
Dayhip Nuñez <dayhip.ti@gmail.com>
2023-07-15 00:43:56 -0400

    class detalleMensualidad

---
+++ src/main/java/dayhipti/controlmonetario/entity/DetalleMensualidad.java
@@ -0,0 +1,22 @@
+package dayhipti.controlmonetario.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table()
+public class DetalleMensualidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_mensualidad_detalle",nullable = false)
+    private int idMensualidadDetalle;
+    @ManyToOne(optional = false,fetch = FetchType.EAGER)
+    @JoinColumn(name = "RUT_usuario",columnDefinition = "varchar(12)")
+    private Usuario usuario;
+    @ManyToOne(optional = false,fetch = FetchType.EAGER)
+    @JoinColumn(name = "ID_mensualidad", columnDefinition = "varchar(10)")
+    private Mes mes;
+    @Column(columnDefinition = "BLOB")
+    private byte[] comprobante;
+}

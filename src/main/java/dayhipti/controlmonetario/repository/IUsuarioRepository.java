27dc5c833fad3c28cb19002338fdeabdc772f3eb
Dayhip Nuñez <dayhip.ti@gmail.com>
2023-07-15 01:30:39 -0400

    usuario v2

--- src/main/java/dayhipti/controlmonetario/repository/IUsuarioRepository.java
+++ src/main/java/dayhipti/controlmonetario/repository/IUsuarioRepository.java
@@ -1,5 +1,8 @@
 package dayhipti.controlmonetario.repository;
 
-public interface IUsuarioRepository {
+import dayhipti.controlmonetario.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUsuarioRepository extends JpaRepository<Usuario,String> {
 
 }
--- src/main/java/dayhipti/controlmonetario/service/IUsuarioService.java
+++ src/main/java/dayhipti/controlmonetario/service/IUsuarioService.java
@@ -1,4 +1,15 @@
 package dayhipti.controlmonetario.service;
 
+import dayhipti.controlmonetario.entity.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service()
 public interface IUsuarioService {
+    Usuario crearUsuario(Usuario usuario);
+    List<Usuario> listaUsuario();
+    Usuario buscarUsuario(int idUsuario);
+    Usuario actualizarUsuario(Usuario usuarioActualizar);
+    void eliminarUsuario(int idUsuario);
 }

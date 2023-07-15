72aebb5e409f941f3141bacad330cf86d9dd112e
dayhip-ti <dayhip.ti@gmail.com>
2023-07-15 02:03:48 -0400

    Service-1

--- src/main/java/dayhipti/controlmonetario/service/serviceinpl/MesImpl.java
+++ src/main/java/dayhipti/controlmonetario/service/serviceinpl/MesImpl.java
@@ -1,12 +1,40 @@
 package dayhipti.controlmonetario.service.serviceinpl;
 
+import dayhipti.controlmonetario.entity.Mes;
 import dayhipti.controlmonetario.repository.IMesRepository;
 import dayhipti.controlmonetario.service.IMesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service("mesImpl")
 public class MesImpl implements IMesService {
     @Autowired
     IMesRepository objMesRepo;
+
+    @Override
+    public Mes crearMes(Mes mes) {
+        return null;
+    }
+
+    @Override
+    public List<Mes> listaMes() {
+        return null;
+    }
+
+    @Override
+    public Mes buscarMes(String idMes) {
+        return null;
+    }
+
+    @Override
+    public Mes actualizarMes(Mes mesActualizar) {
+        return null;
+    }
+
+    @Override
+    public void eliminarMes(String idMes) {
+
+    }
 }
